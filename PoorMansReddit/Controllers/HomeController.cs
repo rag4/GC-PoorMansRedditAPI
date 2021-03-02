d12b648d7097cc1f280298cc8d06359be3938c68
Ramon <rag4@pdx.edu>
2021-03-02 06:03:29 -0800

    initial commit

---
+++ PoorMansReddit/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PoorMansReddit.Models;
+
+namespace PoorMansReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private RedditDAL rd = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowResults()
+        {
+            Rootobject ro = rd.GetPost();
+            List<Child> posts = ro.data.children.ToList();
+            return View(posts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PoorMansReddit/Models/RedditDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PoorMansReddit.Models
+{
+    public class RedditDAL
+    {
+        public string GetData()
+        {
+            string url = @"https://www.reddit.com/r/aww/.json";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string JSON = rd.ReadToEnd();
+            return JSON;
+        }
+
+        public Rootobject GetPost()
+        {
+            string link = GetData();
+            Rootobject ro = JsonConvert.DeserializeObject<Rootobject>(link);
+
+            return ro;
+        }
+    }
+}
